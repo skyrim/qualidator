2e4ae07479629c93ebc1320fd9a6652cae9c3652
Stefan Stojkovic <skyrim@live.com>
2021-05-26 14:41:50 +0300

    fix num/float test

--- src/validators/num/float.test.ts
+++ src/validators/num/float.test.ts
@@ -11,7 +11,14 @@ describe('float', () => {
     expect(float('1.1')).toBe(true)
     expect(float('42')).toBe(false)
     expect(float('hello')).toBe(false)
-    expect(float(undefined)).toBe(false)
+  })
+
+  test('invalid values', () => {
+    expect(float(null as any)).toBe(false)
+    expect(float(undefined as any)).toBe(false)
+    expect(float(true as any)).toBe(false)
+    expect(float(false as any)).toBe(false)
+    expect(float({} as any)).toBe(false)
   })
 
   test('test alias', () => {
